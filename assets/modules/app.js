39daaf2a52fa8607eea8b67a594fdb1b3b9d3baa
wes <wesstep1315@gmail.com>
2021-04-30 10:04:20 -0600

    Started refactoring and improving the drag functionality

--- assets/modules/app.js
+++ assets/modules/app.js
@@ -3,16 +3,20 @@ const POST_COUNT = 3;
 const PLAY_AREA_COUNT = 3;
 
 let draggedRing;
-
+let active = false;
+let currentX;
+let currentY;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
 let rings = [];
-initRings();
-
 let posts = [];
-initPosts();
-
 let playAreas = [];
-initPlayAreas();
 
+initRings();
+initPosts();
+initPlayAreas();
 initDraggableElements();
 
 function initRings() {
@@ -37,19 +41,38 @@ function initPlayAreas() {
 }
 
 function initDraggableElements() {
-	rings[0].setAttribute('draggable', true);
-	document.addEventListener('drag', () => {});
-	document.addEventListener('dragstart', (e) => {
-		draggedRing = e.target;
-	});
-	document.addEventListener('dragend', (e) => {
-		e.preventDefault();
-	});
-	document.addEventListener('drop', moveRing);
+	rings[0].classList.add('draggable');
+	// Init drag events for ring 1
+	rings[0].addEventListener("touchstart", dragStart, false);
+	rings[0].addEventListener("touchend", dragEnd, false);
+	rings[0].addEventListener("touchmove", drag, false);
+
+	rings[0].addEventListener("mousedown", dragStart, false);
+	rings[0].addEventListener("mouseup", dragEnd, false);
+	rings[0].addEventListener("mousemove", drag, false);
 }
 
-function moveRing(e) {
-	e.preventDefault();
+const ringIsDraggable = e => rings.includes(e.target) && e.target.classList.contains('draggable');
+
+function dragStart(e) {
+	if (!ringIsDraggable(e)) {
+		return;
+	}
+
+	active = true;
+	draggedRing = e.target;
+
+	if (e.type === 'touchstart') {
+		initialX = e.touches[0].clientX - xOffset;
+		initialY = e.touches[0].clientY - yOffset;
+	} else {
+		initialX = e.clientX - xOffset;
+		initialY = e.clientY - yOffset;
+	}
+}
+
+function dragEnd(e) {
+	console.log(e.target);
 	if (e.target.className === 'play-area' || e.target.className === 'post') {
 		const origin = draggedRing.parentNode;
 		const destination = (e.target.className === 'play-area') ? e.target : e.target.parentElement;
@@ -58,5 +81,27 @@ function moveRing(e) {
 			origin.children[1].setAttribute('draggable', true);
 		}
 		destination.firstElementChild.after(draggedRing);
+	}
+}
+
+const setTranslate = (xPos, yPos, el) => el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+
+
+function drag(e) {
+	if (active) {
+		e.preventDefault();
+
+		if (e.type === 'touchmove') {
+			currentX = e.touches[0].clientX - initialX;
+			currentY = e.touches[0].clientY - initialY;
+		} else {
+			currentX = e.clientX - initialX;
+			currentY = e.clientY - initialY;
+		}
+
+		xOffset = currentX;
+		yOffset = currentY;
+
+		setTranslate(currentX, currentY, draggedRing);
 	}
 }
